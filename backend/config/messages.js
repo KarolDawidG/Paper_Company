6b19320c6e2b834ee21d3ac0dee299dd96cf4994
KarolDawidG <karoldawidg@gmail.com>
2024-03-01 11:41:59 +0100

    new better messages - after postman test

--- backend/config/messages.js
+++ backend/config/messages.js
@@ -1,49 +1,38 @@
 const MESSAGES = {
   // Successful operations
-  SERVER_STARTED: "Server Started correctly on port",
-  SUCCESSFUL_OPERATION: "Operation successful. Congratulations!",
-  SUCCESSFUL_LOGOUT: "Logout completed successfully. Farewell!",
-  SUCCESSFUL_RESET: "Password has been reset successfully.",
-  SUCCESSFUL_SIGN_UP:
-    "Welcome aboard! You are now officially part of our community.",
-  EMAIL_SUCCESS:
-    "If the email address exists, a password reset link will be sent.",
+  SERVER_STARTED: "Serwer pomyślnie uruchomiony na porcie",
+  SUCCESSFUL_OPERATION: "Operacja zakończona sukcesem.",
+  SUCCESSFUL_LOGOUT: "Wylogowanie zakończone powodzeniem.",
+  SUCCESSFUL_RESET: "Hasło zostało pomyślnie zresetowane.",
+  SUCCESSFUL_SIGN_UP: "Rejestracja zakończona sukcesem. Witamy w naszej społeczności.",
+  EMAIL_SUCCESS: "Link do resetowania hasła został wysłany na podany adres email, o ile znajduje się on w naszych rekordach.",
 
   // Errors and validation messages
-  INCORRECT_USERNAME: "The username should contain at least 6 characters.",
-  ERROR_GET_CONNECTION:
-    "An unexpected glitch occurred while attempting to connect.",
-  SESSION_EXPIRED:
-    "Your session has expired. Please log in again to continue your adventure.",
-  USER_NOT_LOGGED_IN: "Hold up! The user is not logged in.",
-  UNPROCESSABLE_ENTITY:
-    "Invalid login details. Please ensure you have entered the correct login and password.",
-  INVALID_EMAIL: "Invalid email address. Please provide a valid one.",
-  INVALID_PASS:
-    "The password should contain at least 8 and max 16 characters, one capital letter and a number!",
-  EMAIL_USER_EXIST:
-    "The provided email and username are already in use. Please choose different ones.",
-  EMAIL_EXIST: "The email address is already taken. Please choose another one.",
-  USER_EXIST: "The username already exists. Please select a different one.",
-  SQL_INJECTION_ALERT:
-    "Attempted SQL Injection attack detected! Security alert raised.",
-  INTERNET_DISCONNECTED:
-    "You seem to have lost internet connection. Please check your connectivity.",
+  INCORRECT_USERNAME: "Nazwa użytkownika musi zawierać przynajmniej 6 znaków.",
+  ERROR_GET_CONNECTION: "Napotkano błąd połączenia.",
+  SESSION_EXPIRED: "Sesja wygasła. Proszę zalogować się ponownie.",
+  USER_NOT_LOGGED_IN: "Działanie odrzucone. Wymagana autentykacja użytkownika.",
+  UNPROCESSABLE_ENTITY: "Nieprawidłowe dane logowania. Proszę sprawdzić swoje dane logowania.",
+  INVALID_EMAIL: "Adres email jest nieprawidłowy. Proszę podać prawidłowy adres email.",
+  INVALID_PASS: "Hasło musi mieć od 8 do 16 znaków, zawierać jedną wielką literę i cyfrę.",
+  EMAIL_USER_EXIST: "Email i nazwa użytkownika już zarejestrowane. Proszę wybrać inne dane logowania.",
+  EMAIL_EXIST: "Adres email jest już w użyciu. Proszę wybrać inny.",
+  USER_EXIST: "Nazwa użytkownika jest już zajęta. Proszę wybrać inną.",
+  SQL_INJECTION_ALERT: "Ostrzeżenie bezpieczeństwa: Wykryto próbę ataku SQL Injection.",
+  INTERNET_DISCONNECTED: "Utracono połączenie z internetem. Proszę sprawdzić ustawienia sieciowe.",
 
   // Server errors and permissions
-  INVALID_REQUEST: "Invalid request to the server.",
-  SERVER_ERROR:
-    "An unknown server error occurred. Please contact your admin for assistance.",
-  UNKNOW_ERROR: "An unknown error occurred.",
-  FORBIDDEN:
-    "Access to this resource is restricted. You do not have the necessary permissions.",
-  CAPTCHA_ERROR: "Error verifying reCAPTCHA",
+  INVALID_REQUEST: "Żądanie jest nieprawidłowe.",
+  SERVER_ERROR: "Napotkano błąd serwera. Proszę skontaktować się z administratorem w celu uzyskania wsparcia.",
+  UNKNOW_ERROR: "Wystąpił nieznany błąd.",
+  FORBIDDEN: "Dostęp zabroniony. Niewystarczające uprawnienia do żądanego zasobu.",
+  CAPTCHA_ERROR: "Nie udało się zweryfikować reCAPTCHA.",
 
   // Authorization
-  JWT_ERROR: "JsonWebTokenError: Invalid signature. Authorization failed.",
-  NO_REFRESH_TOKEN: "Refresh token missing.",
-  INVALID_REFRESH_TOKEN: "Invalid refresh token provided.",
-  AUTHORIZATION_LVL: "Authorization level: ",
+  JWT_ERROR: "Autoryzacja nie powiodła się z powodu nieprawidłowego podpisu JsonWebToken.",
+  NO_REFRESH_TOKEN: "Brak tokenu odświeżania.",
+  INVALID_REFRESH_TOKEN: "Token odświeżania jest nieprawidłowy.",
+  AUTHORIZATION_LVL: "Poziom autoryzacji: ",
 };
 
 module.exports = MESSAGES;
