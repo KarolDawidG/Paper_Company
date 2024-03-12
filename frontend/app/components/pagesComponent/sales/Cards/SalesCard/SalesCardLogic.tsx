05d450fa54aa42b5381d77e57fb95dd0bbaf2b39
KarolDawidG <karoldawidg@gmail.com>
2024-03-12 19:00:36 +0100

    a formData object established

--- frontend/app/components/pagesComponent/sales/Cards/SalesCard/SalesCardLogic.tsx
+++ frontend/app/components/pagesComponent/sales/Cards/SalesCard/SalesCardLogic.tsx
@@ -1,7 +1,10 @@
-import React, { useState } from "react";
+import { formatDate } from "@/app/components/helpers/formDate";
+import React, { useState, useEffect } from "react";
 
 const SalesCardLogic = () => {
   const [expanded, setExpanded] = useState(false);
+  const [salesId, setSalesId] = useState<string | null>(null);
+  const [transactionDate, setTransactionDate] = useState<string | any>();
   const [formData, setFormData] = useState({
     imie: "",
     email: "",
@@ -13,14 +16,43 @@ const SalesCardLogic = () => {
     nr_mieszkania: "",
     kod: "",
     nazwa_frimy: "",
+    salesId: "",
+    transactionDate:"",
   });
+
+
+  useEffect(() => {
+    const idUser = localStorage.getItem('idUser');
+    if (idUser) {
+      setSalesId(idUser);
+      setFormData(prevData => ({
+        ...prevData,
+        salesId: idUser 
+      }));
+    }
+
+    const currendDate = new Date().toISOString().slice(0, 16);
+    const date = formatDate(currendDate);
+    if (date) {
+      setTransactionDate(date);
+      setFormData(prevData => ({
+        ...prevData,
+        transactionDate: date 
+      }));
+    }
+    
+    
+  }, []);
+
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     console.log(formData);
   };
+
   const handleChange = (field:string | number, value:string | number) => {
     setFormData((prevData) => ({
       ...prevData,
